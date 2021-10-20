687fdce56d161aeb2d806c7b55bad65c86c7e5aa
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2021-10-20 11:58:29 +0200

    He puesto la palabra a adivinar

--- JuegoAhorcado/MainWindow.xaml.cs
+++ JuegoAhorcado/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace JuegoAhorcado
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int NUM_FILAS = 3;
+        private const int NUM_COLUMNAS = 9;
+        private List<string> listaLetras = new List<string>();
+        private List<string> listaPalabras = new List<string>() {"Jirafa", "Ascensor", "Fiesta", "Ahorcado", "Espectaculo"};
+        private string palabraAAdivinar;
         public MainWindow()
         {
             InitializeComponent();
+            AñadirLetraALista();
+            CrearLetra();
+            VisualizacionPalabraAAdivinar();
         }
+        private void CrearLetra()
+        {
+            foreach (string l in listaLetras)
+            {
+                Button boton = new Button();
+                boton.Margin = new Thickness(5);
+                boton.Content = l;
+                uniformGridLetras.Children.Add(boton);
+            }
+        }
+        private void AñadirLetraALista()
+        {
+            for (int i = 65; i <=90; i++)
+            {
+                listaLetras.Add(Convert.ToChar(i).ToString());
+            }
+        }
+        private void VisualizacionPalabraAAdivinar()
+        {
+            int numeroGuiones;
+            Random seed = new Random();
+            for (int i = 0; i < seed.Next(0,6); i++)
+                palabraAAdivinar = listaPalabras[i]; 
+
+            numeroGuiones = palabraAAdivinar.Length;
+            for (int i = 0; i < numeroGuiones; i++)
+            {
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text += "_ ";
+                textBlock.FontSize = 24;
+                wrapPanelPalabraAAdivinar.Children.Add(textBlock);
+            }
+        }
+        
     }
 }
